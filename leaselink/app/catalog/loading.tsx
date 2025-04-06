c70b7d888aeba9b1b827d6907a7e776c817d3294
justinxyy <144742125+justinxyy@users.noreply.github.com>
2025-04-06 17:39:38 -0400

    Delete leaselink directory

--- leaselink/app/catalog/loading.tsx
+++
@@ -1,8 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
-      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary"></div>
-    </div>
-  )
-}
-
